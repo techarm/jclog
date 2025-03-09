94d0a357201704fba622e1e3451bf5a0d650214b
techarm <100292270+techarm@users.noreply.github.com>
2025-03-09 15:35:09 +0900

    fix: update version (#12)

--- cmd/version.go
+++ cmd/version.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/techarm/jclog/internal/meta"
+	"github.com/techarm/jclog/internal"
 	"github.com/urfave/cli/v3"
 )
 
@@ -14,7 +14,7 @@ func NewVersionCommand() *cli.Command {
 		Name:  "version",
 		Usage: "Show version information",
 		Action: func(context.Context, *cli.Command) error {
-			fmt.Println("jclog version", meta.GetMetadata().Version)
+			fmt.Println("jclog version", internal.Version)
 			return nil
 		},
 	}
